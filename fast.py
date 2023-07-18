274ae8d3b6b10a5a8659b89b971704e51e38d6c9
Deepanshu <deepanshu.sagar@pubmatic.com>
2023-07-18 17:35:43 +0530

    usage of bito

--- fast.py
+++ fast.py
@@ -1,25 +1,46 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import json
-import os
+import os,time
 import subprocess
 import tempfile
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 from typing import Any, Dict
 
 class Input(BaseModel):
     prompt: str
     featureName: str
 
+
 def run_cmd(prompt_text):
-    with tempfile.NamedTemporaryFile(mode='w+') as temp:
-        print (prompt_text)
-        temp.write(prompt_text)
-        temp.seek(0)
-        cmd = ["cat", temp.name, "|", "bito"]
-        print (cmd)
-        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-    return output
+    start_time = time.time()
+
+    # Create a temporary file and write the prompt_text into it
+    prompt_fd, prompt_path = tempfile.mkstemp()
+    pyfile_path="tmp"
+    try:
+        with os.fdopen(prompt_fd, 'w') as prompt_file:
+            print (f"{prompt_text}")
+            prompt_file.write(prompt_text)
+        with open(pyfile_path, 'w') as prompt_file:
+            print ("")
+            prompt_file.write("prompt_text")
+        
+        p2 = subprocess.Popen(["bito", "-p", prompt_path, "-f", pyfile_path], stdout=subprocess.PIPE)  # added stdout=subprocess.PIPE
+        output, _ = p2.communicate()  # Get output once here
+    except subprocess.CalledProcessError as e:
+        print(f"Subprocess returned error: {e.output}")
+        output = e.output
+    finally:
+        # Ensure the temporary file is deleted even if an error occurs
+        os.unlink(prompt_path)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return output  # Return the stored output, decoded from bytes to string
+
+
 
 def prompt_to_query_new(prompt: str, info: str, data: str):
     template = """
@@ -43,4 +64,5 @@ async def process(input: Input):
     info = read_file( f"{input.featureName}_info.json")
     data = read_file( f"{input.featureName}_data.txt")
     query = prompt_to_query_new(input.prompt, info, data)
-    return {"Generated SQL Query": query}
+    query_dict = json.loads(query)
+    return {"Generated SQL Query": query_dict}
